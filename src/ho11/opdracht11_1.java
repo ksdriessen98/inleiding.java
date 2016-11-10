e1023ddd3d756309c801819ca80366b71dcb53d6
ksdriessen98 <Shawn2535>
2016-11-10 12:47:39 +0100

    opdracht11_1 af

---
+++ src/ho11/opdracht11_1.java
@@ -0,0 +1,22 @@
+package ho11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * Created by keanu driessen on 10-11-2016.
+ */
+public class opdracht11_1 extends Applet {
+
+    public void init() {}
+
+    public void paint(Graphics g) {
+        int lijnen;
+        int y = 0;
+
+        for(lijnen = 1; lijnen <= 10; lijnen++) {
+            y += 20;
+            g.drawLine(y , 50, y, 250 );
+        }
+    }
+}
